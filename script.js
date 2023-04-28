3e22256e7312fc980383332b89f9b5735af11ba4
Souvik Guha <svkguha123@gmail.com>
2023-04-28 13:49:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: "Rahul Sharma"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+}
+
+// Test getKeys() method on student object
+console.log(student.getKeys()); // Output: ["name"]
